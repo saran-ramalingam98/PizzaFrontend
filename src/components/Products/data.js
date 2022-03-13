b16c811a8d762d102a1eae55bbdcbce3ad9191c2
saran-ramalingam98 <98377456+saran-ramalingam98@users.noreply.github.com>
2022-03-13 09:25:19 +0530

    Update data.js

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,66 +1,60 @@
-import product1 from '../../images/product-1.jpg';
-import product2 from '../../images/product-2.jpg';
-import product3 from '../../images/product-3.jpg';
-import sweet1 from '../../images/sweet3.jpg';
-import sweet2 from '../../images/sweet-2.jpg';
-import sweet3 from '../../images/sweet-3.jpg';
+import product1 from "../../images/product-1.jpg";
+import product2 from "../../images/product-2.jpg";
+import product3 from "../../images/product-3.jpg";
+import product4 from "../../images/pizza-1.jpg";
+import product5 from "../../images/pizza-2.jpg";
+import product6 from "../../images/pizza-3.jpg";
 
 export const productData = [
   {
     img: product1,
-    alt: 'Pizza',
-    name: 'Supreme Pizza',
-    desc:
-      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-    price: '$19.99',
-    button: 'Add to Cart'
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "₹199",
+    button: "Add to Cart",
   },
   {
     img: product2,
-    alt: 'Pizza',
-    name: 'Hawaiian Paradise',
-    desc:
-      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-    price: '$16.99',
-    button: 'Add to Cart'
+    alt: "Pizza",
+    name: "Hawaiian Paradise",
+    desc: " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "₹250",
+    button: "Add to Cart",
   },
   {
     img: product3,
-    alt: 'Pizza',
-    name: 'Veggie Overload',
-    desc:
-      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
-    price: '$14.99',
-    button: 'Add to Cart'
-  }
+    alt: "Pizza",
+    name: "Veggie Overload",
+    desc: " Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "₹300",
+    button: "Add to Cart",
+  },
 ];
 
 export const productDataTwo = [
   {
-    img: sweet2,
-    alt: 'Donuts',
-    name: 'Doughlicious',
-    desc:
-      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
-    price: '$9.99',
-    button: 'Add to Cart'
+    img: product4,
+    alt: "Pizza",
+    name: "Sicilian pizza",
+    desc: "Sicilian pizza, also known as sfincione, provides a thick cut of pizza with pillowy dough, a crunchy crust, and robust tomato sauce.",
+    price: "₹500",
+    button: "Add to Cart",
   },
   {
-    img: sweet3,
-    alt: 'Ice Cream',
-    name: 'Caramel Wonder',
-    desc:
-      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
-    price: '$12.99',
-    button: 'Add to Cart'
+    img: product5,
+    alt: "Pizza",
+    name: "New York-Style Pizza",
+    desc: "It's characteristic large, foldable slices and crispy outer crust, New York-style pizza is one of America’s most famous regional pizza types.",
+    price: "₹450",
+    button: "Add to Cart",
   },
   {
-    img: sweet1,
-    alt: 'Brownie',
-    name: 'Brownie Bunch',
-    desc:
-      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
-    price: '$9.99',
-    button: 'Add to Cart'
-  }
+    img: product6,
+    alt: "Pizza",
+    name: "Chicago Pizza",
+    desc: "In order to easily get a Chicago pizza out of the pan, it's important to wipe the pan down with oil, with a baking time of 30 - 35 minutes.",
+    price: "₹550",
+    button: "Add to Cart",
+  },
 ];
